e92b1d3e8460121ad01458833b9a33ca1ca57ea8
bananaboat <937499953@qq.com>
2023-04-02 13:31:16 +0800

    update: test creack/pty

--- pty-docker/base-pty/main.go
+++ pty-docker/base-pty/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	_ "github.com/creack/pty"
-	_ "google.golang.org/grpc"
+	"os/exec"
+	"time"
+
+	"github.com/creack/pty"
 )
 
 func main() {
+	c := exec.Command("python")
+	f, _ := pty.Start(c)
+	defer f.Close()
+	time.Sleep(time.Second)
+
+	f.Write([]byte("print('hello')\n"))
+	f.Write([]byte{4}) // EOT
+	// go func() {
+	// 	f.Write([]byte("ls\n"))
+	// 	f.Write([]byte{4}) // EOT
+	// }()
+
 }
